e9dce971b1a4365d3c671272a6049d6c113e6c4b
Felps <felipinlineaps@gmail.com>
2012-06-01 01:05:03 -0300

    novas medidas adicionadas

--- Configurador/src/utils/MedidasPadroes.java
+++ Configurador/src/utils/MedidasPadroes.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 /**
  *
@@ -16,4 +17,6 @@ public class MedidasPadroes {
     public static final Dimension TAMANHO_TELA_PRIMEIRA_CONFIGURACAO = new Dimension(580,380);
     public static final Dimension TAMANHO_TELA_LOGIN = new Dimension(500,370);
     public static final Color COR_DE_FUNDO = new Color(91, 105, 214);
+    public static final Color COR_LABEL = Color.LIGHT_GRAY;
+    public static final Font FONTE = new Font(Font.SANS_SERIF, Font.BOLD, 12);
 }
