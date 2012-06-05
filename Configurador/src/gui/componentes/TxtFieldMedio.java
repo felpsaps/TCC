d69758231fbab5ef52ca749aefa3fbfdd178878e
Felps <felipinlineaps@gmail.com>
2012-06-05 20:40:50 -0300

    txtfield medio padrao

---
+++ Configurador/src/gui/componentes/TxtFieldMedio.java
@@ -0,0 +1,15 @@
+package gui.componentes;
+
+import javax.swing.JTextField;
+import utils.MedidasPadroes;
+
+/**
+ *
+ * @author Felps
+ */
+public class TxtFieldMedio extends JTextField{
+    
+    public TxtFieldMedio() {
+        setPreferredSize(MedidasPadroes.MEDIDA_TEXTFIELD_MEDIO);
+    }
+}
