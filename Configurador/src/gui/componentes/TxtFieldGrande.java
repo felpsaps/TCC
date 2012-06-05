bab767f04e23d2633c56800ff71cfcf02d82d000
Felps <felipinlineaps@gmail.com>
2012-06-05 20:41:16 -0300

    txtfield grande padrao

---
+++ Configurador/src/gui/componentes/TxtFieldGrande.java
@@ -0,0 +1,15 @@
+package gui.componentes;
+
+import javax.swing.JTextField;
+import utils.MedidasPadroes;
+
+/**
+ *
+ * @author Felps
+ */
+public class TxtFieldGrande extends JTextField{
+    
+    public TxtFieldGrande() {
+        setPreferredSize(MedidasPadroes.MEDIDA_TEXTFIELD_GRANDE);
+    }
+}
