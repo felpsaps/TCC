db122a747e2ca36f8f877726d606477b6c31f876
Felps <felipinlineaps@gmail.com>
2012-06-05 20:39:56 -0300

    novas medidas adicionadas

--- Configurador/src/utils/MedidasPadroes.java
+++ Configurador/src/utils/MedidasPadroes.java
@@ -11,12 +11,10 @@ import java.awt.Font;
 public class MedidasPadroes {
     public static final Dimension MEDIDA_TEXTFIELD_GRANDE = new Dimension(200,28);
     public static final Dimension MEDIDA_TEXTFIELD_MEDIO = new Dimension(100,28);
-    public static final Dimension MEDIDA_BTNOK = new Dimension(30,28);
-    public static final Dimension MEDIDA_BTNLOGIN = new Dimension(65,28);
-    public static final Dimension MEDIDA_BTNCANCELAR = new Dimension(85,28);
+    public static final Dimension MEDIDA_BTN_PADRAO = new Dimension(85,28);
     public static final Dimension TAMANHO_TELA_PRIMEIRA_CONFIGURACAO = new Dimension(580,380);
     public static final Dimension TAMANHO_TELA_LOGIN = new Dimension(500,370);
     public static final Color COR_DE_FUNDO = new Color(91, 105, 214);
     public static final Color COR_LABEL = Color.LIGHT_GRAY;
-    public static final Font FONTE = new Font(Font.SANS_SERIF, Font.BOLD, 12);
+    public static final Font FONTE_LABELS = new Font(Font.SANS_SERIF, Font.BOLD, 12);
 }
